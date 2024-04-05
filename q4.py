56e75d47695287e34718c4de70c28980eb873967
Alice Cruz <127883364+aliceczr@users.noreply.github.com>
2024-04-05 20:39:02 -0300

    Delete q4.py

--- q4.py
+++
@@ -1,6 +0,0 @@
-def separar_digitos(numero):
-    digitos = [int(digito) for digito in str(numero)]
-    return " ".join(map(str, digitos))
-
-numero = int(input("Digite um número: "))
-print(separar_digitos(numero))
