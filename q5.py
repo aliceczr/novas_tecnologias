9218b1d1537b58da3fc05d480884393dfbc7631c
Alice Cruz <127883364+aliceczr@users.noreply.github.com>
2024-04-05 20:41:58 -0300

    Delete q5.py

--- q5.py
+++
@@ -1,6 +0,0 @@
-def separar_digitos(numero):
-    digitos = [digito for digito in str(numero) if digito.isdigit()]
-    return " ".join(digitos)
-
-numero = input("Digite um número: ")
-print(separar_digitos(numero))
